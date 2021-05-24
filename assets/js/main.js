68b6b923e2825cd8800dfdb3387d8cc2a537bd11
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-05-24 21:24:28 +0600

    start making responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,17 @@ const loader = document.querySelector('.loader');
 window.addEventListener('load', ()=>{
 loader.classList.add('off');
 })
+
+const nav = document.querySelector('nav');
+
+window.addEventListener('scroll', sticky);
+
+function sticky(){
+    if(window.pageYOffset > 100){
+        nav.classList.add('scroll');
+    } else{
+        nav.classList.remove('scroll');
+    }
+}
+
+const menu = document.querySelector('.icon');
